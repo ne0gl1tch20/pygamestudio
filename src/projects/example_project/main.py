15694692a7ac7edaac010a72965007aef9de4a22
ne0gl1tch20 <ioanjaredt@gmail.com>
2025-11-20 18:56:22 +0800

    eyy

--- src/projects/example_project/main.py
+++
@@ -1,7 +0,0 @@
-# Example project main.py
-def start_game(game_manager):
-    print("--- Example Project Started ---")
-    return {"name": "Main Scene", "objects": [], "is_3d": False}
-
-def update_game(game_manager, dt):
-    pass
